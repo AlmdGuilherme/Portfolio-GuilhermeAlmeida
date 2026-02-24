a87abecfbc4a53a310bd52030864186c7a33524a
AlmdGuilherme <guilhermecamargo_@outlook.com>
2026-02-24 20:57:26 -0300

    build: otimização de chunks e limites do vite

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,11 +5,18 @@ export default defineConfig({
   plugins: [react()],
   base: `/`, 
   build: {
+    chunkSizeWarningLimit: 1600, 
     rollupOptions: {
       output: {
         assetFileNames: 'assets/[name].[hash][extname]',
         chunkFileNames: 'assets/[name].[hash].js',
         entryFileNames: 'assets/[name].[hash].js',
+
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
       },
     },
   },
